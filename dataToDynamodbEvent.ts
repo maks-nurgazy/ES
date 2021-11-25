d0210e280e813b944d451f2d686d9b8ad283b10b
Maksatbek Bolushov <maksatbek.bolushov@gmail.com>
2021-11-26 03:39:38 +0600

    BULK mapping completed

---
+++ dataToDynamodbEvent.ts
@@ -0,0 +1,58 @@
+const data = [
+  {
+    id: '20211021_R4567GT',
+    arrivalPurpose: 'CARGO_IMPORT',
+    netWeight: 0,
+    status: 'ARRIVED',
+    borderCheckpoint: '',
+    driver: {
+      name: 'Anyone',
+    },
+    operator: {
+      id: '093c7baf-896b-4fd0-bc05-253eb7fac606',
+      organization: {
+        name: '',
+        id: '53ddbae9-0d27-420a-b692-6221e734b194',
+      },
+    },
+    tareWeight: 0,
+    departureCountry: 'China',
+    organization: {
+      name: '',
+      id: '53ddbae9-0d27-420a-b692-6221e734b194',
+    },
+    weightType: 'GROSS',
+    vehiclePlateNumber: 'R4567GT',
+    grossWeight: 300,
+    arrivalDate: 1634806747244,
+  },
+  {
+    id: '20211003_05KG998AAO',
+    arrivalPurpose: 'CARGO_IMPORT',
+    netWeight: 0,
+    status: 'DEPARTED',
+    borderCheckpoint: 'Irkeshtam',
+    driver: {
+      name: 'Шакенов Э',
+    },
+    operator: {
+      id: '093c7baf-896b-4fd0-bc05-253eb7fac606',
+      organization: {
+        name: '',
+        id: '53ddbae9-0d27-420a-b692-6221e734b194',
+      },
+    },
+    tareWeight: 0,
+    departureCountry: 'China',
+    organization: {
+      name: '',
+      id: '53ddbae9-0d27-420a-b692-6221e734b194',
+    },
+    weightType: 'GROSS',
+    departureDate: 1633286561819,
+    vehiclePlateNumber: '05KG998AAO',
+    grossWeight: 0,
+    arrivalDate: 1633282315813,
+  },
+];
+
--- indexMapperBulk.ts
+++ indexMapperBulk.ts
@@ -78,6 +78,8 @@ export type Vehicle = {
 };
 
 export async function handler(event: DynamoDBStreamEvent) {
+  const bulkData = [];
+
   for (let record of event.Records) {
     const { eventSourceARN, eventName, dynamodb } = record;
     if (!eventSourceARN || !eventName || !dynamodb) continue;
@@ -96,13 +98,7 @@ export async function handler(event: DynamoDBStreamEvent) {
 
       const newVehicle = DynamoDB.Converter.unmarshall(NewImage) as Vehicle;
 
-      const insertResponse = await esClient.index({
-        index: getVehicleIndex(newVehicle, tableName),
-        id: newVehicle.id,
-        body: newVehicle,
-      });
-
-      printResponse(insertResponse);
+      bulkData.push(...createIndex(getHotIndex(tableName), newVehicle));
     }
 
     // Update event
@@ -116,29 +112,45 @@ export async function handler(event: DynamoDBStreamEvent) {
         newVehicle.status === VehicleStatus.DEPARTED &&
         oldVehicle.status === VehicleStatus.ARRIVED
       ) {
-        await Promise.all([
-          esClient.delete({
-            index: getHotIndex(tableName),
-            id: newVehicle.id,
-          }),
-          esClient.index({
-            index: getColdIndex(tableName),
-            id: newVehicle.id,
-            body: newVehicle,
-          }),
-        ]);
+        bulkData.push(deleteIndex(getHotIndex(tableName), newVehicle.id));
+        bulkData.push(...createIndex(getColdIndex(tableName), newVehicle));
       } else {
-        const updateResponse = await esClient.update({
-          index: getVehicleIndex(newVehicle, tableName),
-          id: newVehicle.id,
-          body: {
-            doc: newVehicle,
-          },
-        });
-
-        printResponse(updateResponse);
+        bulkData.push(
+          ...updateIndex(getVehicleIndex(newVehicle, tableName), newVehicle)
+        );
       }
     }
+  }
+
+  const data = await esClient.bulk({ body: bulkData, refresh: true });
+  printResponse(data);
+
+  function updateIndex(index: string, data: Vehicle): any {
+    return [
+      {
+        delete: { _index: index, _id: data.id },
+      },
+      { doc: data },
+    ];
+  }
+
+  function deleteIndex(index: string, id: string): {} {
+    return {
+      delete: { _index: index, _id: id },
+    };
+  }
+
+  /*
+  index: If the document exists, replaces the document and increments the version.
+  create: Indexes the specified document if it does not already exist.
+  */
+  function createIndex(index: string, data: Vehicle): any {
+    return [
+      {
+        index: { _index: index, _id: data.id },
+      },
+      data,
+    ];
   }
 
   function getVehicleIndex(vehicle: Vehicle, tableName: string) {
@@ -172,6 +184,84 @@ const hotArrivedCargoImport: StreamRecord = {
   NewImage: {
     id: {
       S: '20211028_05KG711ABJ',
+    },
+    arrivalPurpose: {
+      S: 'CARGO_IMPORT',
+    },
+    netWeight: {
+      N: '0',
+    },
+    trailerPlateNumber: {
+      S: '05KG997PA',
+    },
+    status: {
+      S: 'ARRIVED',
+    },
+    borderCheckpoint: {
+      S: 'Torugart',
+    },
+    operator: {
+      M: {
+        id: {
+          S: 'fab1a253-4b7e-4269-adbf-b3c141d99dac',
+        },
+        organization: {
+          M: {
+            id: {
+              S: '61ba7c84-1ad5-4a37-88a9-15892b58be22',
+            },
+          },
+        },
+      },
+    },
+    driver: {
+      M: {
+        name: {
+          S: 'Токтобаев М',
+        },
+      },
+    },
+    tareWeight: {
+      N: '0',
+    },
+    departureCountry: {
+      S: 'China',
+    },
+    organization: {
+      M: {
+        id: {
+          S: '61ba7c84-1ad5-4a37-88a9-15892b58be22',
+        },
+      },
+    },
+    weightType: {
+      S: 'GROSS',
+    },
+    vehiclePlateNumber: {
+      S: '05KG711ABJ',
+    },
+    grossWeight: {
+      N: '0',
+    },
+    arrivalDate: {
+      N: '1635413689730',
+    },
+  },
+  SequenceNumber: '13021600000000001596893679',
+  SizeBytes: 112,
+  StreamViewType: 'NEW_AND_OLD_IMAGES',
+};
+
+const createData: StreamRecord = {
+  ApproximateCreationDateTime: 1479499740,
+  Keys: {
+    id: {
+      S: 'maks',
+    },
+  },
+  NewImage: {
+    id: {
+      S: 'maks',
     },
     arrivalPurpose: {
       S: 'CARGO_IMPORT',
@@ -312,6 +402,72 @@ const coldCargoImport: StreamRecord = {
       N: '1635413689730',
     },
   },
+  OldImage: {
+    id: {
+      S: '20211028_05KG711ABJ',
+    },
+    arrivalPurpose: {
+      S: 'CARGO_IMPORT',
+    },
+    netWeight: {
+      N: '0',
+    },
+    trailerPlateNumber: {
+      S: '05KG997PA',
+    },
+    status: {
+      S: 'ARRIVED',
+    },
+    borderCheckpoint: {
+      S: 'Torugart',
+    },
+    operator: {
+      M: {
+        id: {
+          S: 'fab1a253-4b7e-4269-adbf-b3c141d99dac',
+        },
+        organization: {
+          M: {
+            id: {
+              S: '61ba7c84-1ad5-4a37-88a9-15892b58be22',
+            },
+          },
+        },
+      },
+    },
+    driver: {
+      M: {
+        name: {
+          S: 'Токтобаев М',
+        },
+      },
+    },
+    tareWeight: {
+      N: '0',
+    },
+    departureCountry: {
+      S: 'China',
+    },
+    organization: {
+      M: {
+        id: {
+          S: '61ba7c84-1ad5-4a37-88a9-15892b58be22',
+        },
+      },
+    },
+    weightType: {
+      S: 'GROSS',
+    },
+    vehiclePlateNumber: {
+      S: '05KG711ABJ',
+    },
+    grossWeight: {
+      N: '0',
+    },
+    arrivalDate: {
+      N: '1635413689730',
+    },
+  },
   SequenceNumber: '13021600000000001596893679',
   SizeBytes: 112,
   StreamViewType: 'NEW_AND_OLD_IMAGES',
@@ -327,6 +483,16 @@ const createRecord: DynamoDBRecord = {
   eventSourceARN: `arn:aws:dynamodb:region:123456789012:table/Vehicles/stream/2016-11-16T20:42:48.104`,
 };
 
+const createRecordData: DynamoDBRecord = {
+  eventID: '7de3041dd709b024af6f29e4fa13d34c',
+  eventName: 'INSERT',
+  eventVersion: '1.1',
+  eventSource: 'aws:dynamodb',
+  awsRegion: 'us-west-2',
+  dynamodb: createData,
+  eventSourceARN: `arn:aws:dynamodb:region:123456789012:table/Vehicles/stream/2016-11-16T20:42:48.104`,
+};
+
 const updateRecord: DynamoDBRecord = {
   eventID: '7de3041dd709b024af6f29e4fa13d3js',
   eventName: 'MODIFY',
@@ -339,7 +505,7 @@ const updateRecord: DynamoDBRecord = {
 
 async function doJob() {
   const data: DynamoDBStreamEvent = {
-    Records: [createRecord, updateRecord],
+    Records: [createRecord, createRecordData, updateRecord],
   };
 
   await handler(data);
